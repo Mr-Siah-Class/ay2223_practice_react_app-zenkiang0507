2b5c73286dea262f88477ff94a044840d197f8a2
zenkiang0507 <zenkiang2004@gmail.com>
2022-07-08 15:05:19 +0800

    Routes with react router DOMS & 404 Screen #4

--- react-app-example/src/App.js
+++ react-app-example/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import {
+  Route,
+  useParams,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+
 import NavBar from './components/NavBar';
+import HomeScreen from './screens/HomeScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import NoScreen from './screens/NoScreen';
+import FooterBar from './components/FooterBar';
+import SingleProductScreen from './screens/SingleProductScreen';
 
 class App extends React.Component
 {
@@ -15,6 +27,16 @@ class App extends React.Component
             <div>
               <NavBar/>
                 <h1>Hello React!</h1>
+                <BrowserRouter>
+                  <Routes>
+                    <Route exact path="/" element={<HomeScreen/>}/>
+                    <Route path="/products" element={<ProductsScreen/>}/>
+                    <Route path="/products/:productId" element={<SingleProductScreen/>}/>
+                    <Route path="*" element={<NoScreen/>}/>
+                  </Routes>
+                </BrowserRouter>
+
+                <FooterBar/>
             </div>
         );
     }
